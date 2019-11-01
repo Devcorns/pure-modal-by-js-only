00e5d0fd0988a94203c9571baf1bbf08c9b05c62
Prakhar Mathur <prakhar@isglobalweb.com>
2019-11-01 00:46:51 -0700

    changes in css

--- assets/js/custom-modal.js
+++ assets/js/custom-modal.js
@@ -1,24 +1,29 @@
 var getModal = document.querySelectorAll('[data-target]');
 
 if(getModal.length) {
-    getModal.forEach(function(data) {
+    getModal.forEach( function( data ) {
         data.addEventListener("click", function(e) {
             e.preventDefault();
             let modalId = e.target.getAttribute("data-target");
             let getModalBox = document.getElementById(modalId);
+            console.log(modalId)
             getModalBox.classList.toggle("d-none");
             getModalBox.addEventListener("click", function(e) {
-                if( e.target.id == modalId) {
+                if( e.target.id == modalId ) {
                     e.target.classList.add("d-none");
                 }
-            })
+            });
         });
     });
 }
 
-let closeIcon = document.querySelectorAll(".close-modal");
-closeIcon.forEach(elm=>{
-    elm.addEventListener("click",function(e) {
-        e.target.closest(".modal").classList.add("d-none");
-    })
-})
+let closeIcon = document.querySelectorAll("[data-target='close-modal']");
+closeIcon.forEach( elm => {
+    elm.addEventListener("click", function(e) {
+        e.target.closest(".modal").classList.add('d-none');
+    });
+});
+
+window.resize = function(e) {
+ this.alert();
+}
